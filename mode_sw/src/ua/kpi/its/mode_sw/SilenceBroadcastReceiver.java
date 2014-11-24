3ffa2c084cdc52378c4c765798d6d739f0967a89
pppimka <pppimka@gmail.com>
2014-11-25 00:34:15 +0200

    Ok

---
+++ mode_sw/src/ua/kpi/its/mode_sw/SilenceBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package ua.kpi.its.mode_sw;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+/**
+ * Created by ann_ on 23.11.2014.
+ */
+public class SilenceBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        audio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+    }
+}
